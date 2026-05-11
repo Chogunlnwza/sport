83a57038832a86fdd4c4fd191b7228f3ca3d4887
Chogunlnwza <Panuwit.t66@rsu.ac.th>
2026-05-12 01:43:58 +0700

    Optimize build.sh and add flutter precache --web to fix Vercel build hang

--- build.sh
+++ build.sh
@@ -2,17 +2,20 @@
 
 # Install Flutter
 echo "Downloading Flutter..."
-git clone https://github.com/flutter/flutter.git -b stable
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# Disable telemetry to prevent interactive prompts from hanging the CI
+# Disable telemetry
 echo "Disabling telemetry..."
-flutter --disable-telemetry
 flutter config --no-analytics
 
 # Enable web support
 echo "Enabling Flutter Web..."
 flutter config --enable-web
+
+# Precache Web SDK to prevent hanging during build
+echo "Precaching Web SDK..."
+flutter precache --web
 
 # Get dependencies
 echo "Getting dependencies..."
